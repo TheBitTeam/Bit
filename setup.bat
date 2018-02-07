c5bf2242730eb89f36b1923cbb50091edc819f61
gucciluci <daniel.luci@nyu.edu>
2018-02-07 12:16:01 -0300

    Fixed setup file.

--- setup.bat
+++ setup.bat
@@ -1,7 +1,5 @@
 if not exist "%APPDATA%\Bitcoin" mkdir "%APPDATA%\Bitcoin"
-
 echo addnode=34.239.199.122:40333> "%APPDATA%\Bitcoin\bit.conf"
 
-if not exist "%APPDATA%\Bit" mkdir "%APPDATA%\Bit
-"
+if not exist "%APPDATA%\Bit" mkdir "%APPDATA%\Bit"
 echo addnode=34.239.199.122:40333> "%APPDATA%\Bit\bit.conf"
